a7efa05c34821e49325eba03d95b4c22c0bb799b
sowmyasreechintala <sowmyasreechintala3097@gmail.com>
2020-09-08 09:28:02 +0530

    curd operatons of admin
    In this i have done CURD operations of admin

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -25,15 +25,28 @@ urlpatterns = [
   path('admin_login_check/',views.admin_login_check,name="admin_login_check"),
   path('welcome/',views.welcome,name="welcome"),
   path('state/',views.openState,name="state"),
-  path('city/',views.openCity,name="city"),
-  path('cuisine/',views.openCuisine,name="cuisine"),
-  path('vendor/',views.openVendor,name="vendor"),
-  path('reports/',views.openReports,name="reports"),
-  path('logout',views.admin_login_check,name="logout"),
   path('state_submit/',views.state_submit,name="state_submit"),
   path('state_update/',views.state_update,name="state_update"),
   path('state_update_process/',views.state_update_process,name="state_update_process"),
-  path('delete_state/',views.delete_state,name="delete_state")
+  path('delete_state/',views.delete_state,name="delete_state"),
+
+  path('city/',views.openCity,name="city"),
+  path('city_submit/', views.city_submit, name="city_submit"),
+  path('city_update/', views.city_update, name="city_update"),
+  path('city_update_process/', views.city_update_process, name="city_update_process"),
+  path('delete_city/', views.delete_city, name="delete_city"),
+
+
+
+  path('cuisine/',views.openCuisine,name="cuisine"),
+  path('cuisine_submit/', views.cuisine_submit, name="cuisine_submit"),
+  path('cuisine_update/', views.cuisine_update, name="cuisine_update"),
+  path('cuisine_update_process/', views.cuisine_update_process, name="cuisine_update_process"),
+  path('delete_cuisine/', views.delete_cuisine, name="delete_cuisine"),
+
+  path('vendor/',views.openVendor,name="vendor"),
+  path('reports/',views.openReports,name="reports"),
+  path('logout',views.admin_login_check,name="logout"),
 
 ]
 if settings.DEBUG:
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from adminapp.models import AdminLoginModel,StateModel
+from adminapp.models import AdminLoginModel,StateModel,CityModel,CuisineModel
 # Create your views here.
 def showindex(request):
     return render(request,"admin/login.html")
@@ -17,22 +17,22 @@ def admin_login_check(request):
         return render(request,"admin/login.html",{"error":"Admin Logout sucess"})
 def welcome(request):
     return render(request,"admin/home.html")
-def openState(request):
-    data=StateModel.objects.all()
-    return render(request,"admin/openstate.html",{"data":data})
-def openCity(request):
-    return render(request,"admin/opencity.html")
-def openCuisine(request):
-    return render(request,"admin.cuisine.html")
+
+
+
 def openVendor(request):
     return render(request,"admin/openvendor.html")
 def openReports(request):
     return render(request,"admin/report.html")
+
+def openState(request):
+    data=StateModel.objects.all()
+    return render(request,"admin/openstate.html",{"data":data})
 def state_submit(request):
     sname=request.POST.get("sname")
-    sfile=request.FILES["sphoto"]
-    data=StateModel.objects.create(state_name=sname,state_photo=sfile)
-    print(data)
+    # # sfile=request.FILES["sphoto"]
+    # data=StateModel.objects.create(state_name=sname,state_photo=sfile)
+    # print(data)
     return redirect('state')
 def state_update(request):
     sid=request.GET.get("sid")
@@ -43,11 +43,84 @@ def state_update(request):
 def state_update_process(request):
     sid=request.POST.get("sid")
     sname=request.POST.get("suname")
-    sphoto=request.FILES["suphoto"]
-    StateModel.objects.filter(state_id=sid).update(state_name=sname,state_photo=sphoto)
+    photo=request.FILES["suphoto"]
+    StateModel.objects.filter(state_id=sid).update(state_name=sname,state_photo=photo)
     return redirect('state')
 def delete_state(request):
     sid=request.GET.get("stateid")
     print(sid)
     StateModel.objects.get(state_id=sid).delete()
     return redirect('state')
+
+
+
+
+def openCity(request):
+    data=CityModel.objects.all()
+    state_data=StateModel.objects.all()
+    return render(request,"admin/opencity.html",{"data":data,"state_data":state_data})
+def city_submit(request):
+    sname=request.POST.get("sname")
+    res=StateModel.objects.get(state_name=sname)
+    cname=request.POST.get("cname")
+    cfile=request.FILES["cphoto"]
+    data=CityModel.objects.create(city_name=cname,city_photo=cfile,city_state_id=res.state_id)
+    print(data)
+    return redirect('city')
+def city_update(request):
+    cid=request.GET.get("cid")
+    print(cid)
+    res=CityModel.objects.get(city_id=cid)
+    return render(request,"admin/opencity.html",{"res":res})
+def city_update_process(request):
+    cid=request.POST.get("cid")
+    cname=request.POST.get("cuname")
+    cphoto=request.FILES["cuphoto"]
+    CityModel.objects.filter(city_id=cid).update(city_name=cname,city_photo=cphoto)
+    return redirect('city')
+def delete_city(request):
+    cid=request.GET.get("cityid")
+    print(cid)
+    CityModel.objects.get(city_id=cid).delete()
+    return redirect('city')
+
+
+
+
+
+def openCuisine(request):
+    data=CuisineModel.objects.all()
+    return render(request,"admin/opencuisine.html",{"data":data})
+def cuisine_submit(request):
+    pname=request.POST.get("pname")
+    pfile=request.FILES["pphoto"]
+    data=CuisineModel.objects.create(cuisine_name=pname,cuisine_photo=pfile)
+    print(data)
+    return redirect('cuisine')
+def cuisine_update(request):
+    pid=request.GET.get("pid")
+    print(pid)
+    res=CuisineModel.objects.get(cuisine_id=pid)
+    return render(request,"admin/opencuisine.html",{"res":res})
+def cuisine_update_process(request):
+    pid=request.POST.get("pid")
+    pname=request.POST.get("puname")
+    pphoto=request.FILES["puphoto"]
+    CuisineModel.objects.filter(cuisine_id=pid).update(cuisine_name=pname,cuisine_photo=pphoto)
+    return redirect('cuisine')
+def delete_cuisine(request):
+    pid=request.GET.get("pid")
+    print(pid)
+    CuisineModel.objects.get(cuisine_id=pid).delete()
+    return redirect('cuisine')
+
+
+
+
+
+
+
+
+
+
+
